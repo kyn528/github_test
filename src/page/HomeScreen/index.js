ce2a20687cba47f3198b3218c928c2e17a6d730a
[ynkang] <[ynkang@ctrip.com]>
2020-02-19 20:30:07 +0800

    finsh welcome page

---
+++ src/page/HomeScreen/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { CreateStackNavogator } from "react-navigation-stack"
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyConyent: 'center',
+        backgroundColor: '#B4EEB4'
+    },
+    textTips: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#AFEEEE'
+    },
+});
+
+class  Home extends Component{
+
+}
---
+++ src/page/WelcomeScreen/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View, Text,StyleSheet } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+
+//开机之后欢迎界面
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFDAB9',
+    },
+    textTips: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#8EE5EE',
+    },
+});
+
+//开机进入欢迎页面1s后跳转至首页
+class Welcome extends Component {
+    componentDidMount() {
+        //应用中的每个页面组件会自动提供this.props.navigation ，可以获取到一些方法
+        setTimeout(() => { this.props.navigation.navigate('Home') }, 1000);
+    }
+
+    render() {
+        return (
+            <Vew style={styles.container}>
+                <Text style={styles.textTips} >音乐的力量</Text>
+            </Vew>
+        );
+
+    }
+}
+
+//StackNavigator:一次只渲染一个页面，并提供页面之间的跳转方法，当打开一个新的页面时，它被放置在堆栈顶部
+//StackNavigator :屏幕上方导航栏
+const WelcomeStackNavigator = createStackNavigator(
+    {
+        Welcome: {
+            Screen: Welcome,
+        }
+    }
+);
+
+export default WelcomeStackNavigator;
